c1a531bb3c346596fadbdbd9e91f6ed4c5ba1def
AhmedHamdy <geoAhmedHamdy1@gmail.com>
2022-09-26 16:35:19 +0200

    Add Middleware for http request login & security

--- src/MVC/Routes/routes.ts
+++ src/MVC/Routes/routes.ts
@@ -13,6 +13,12 @@ routes.get("/", (req: Request, res: Response) => {
 `);
 });
 
+routes.post("/", (req: Request, res: Response) => {
+  console.log(req.body);
+
+  res.status(200).json({ msg: "Hello Ya Man 😥💔", data: req.body });
+});
+
 routes.use(test_route);
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,14 @@ import {} from "dotenv/config";
 import express, { Application, Request, Response } from "express";
 import morgan from "morgan";
 import cors from "cors";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 import routes from "./MVC/Routes/routes";
 
+/**----------------------**
+ * Initiate Express Server
+ */
 const app: Application = express();
 
 const port = process.env.PORT || 8080;
@@ -14,13 +19,29 @@ app.listen(port, () => {
   console.log(`Server listening on: http://localhost:${port}`);
 });
 
+/**-------------------**
+ *  Helper Variables
+ */
+
+const limiter = rateLimit({
+  windowMs: 60 * 1000, // 1 minute
+  max: 3, // Limit each IP
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+  message: "Too many request to this IP, please try again after 1 minute! 😥🤷‍♀️",
+});
+
 /**-----------------------**
  *  Endpoints
  */
+
 app.use(express.static("public"));
-// app.use(express.json());
+app.use(express.json()); // parses incoming requests with JSON payloads
 // app.use(express.urlencoded({ extended: false }));
 
+// Use to limit repeated requests to public APIs and/or endpoints
+app.use(limiter);
+app.use(helmet()); // to Securing http requests
 app.use(morgan("dev"));
 app.use(cors());
 app.use(routes);
